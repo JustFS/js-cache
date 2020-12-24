5783698e1290f955525d9fe918e08f53961e3368
Julien Az <julien.azbrg@gmail.com>
2020-12-24 16:40:44 +0100

    finished 1.0

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+const cacheName = "cache-v1";
+const assets = ["/", "/index.html", "/img.jpg", "/style.css"];
+
+// mettre en cache
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// fetcher le cache
+self.addEventListener("fetch", (e) => {
+  // console.log(e.request);
+  e.respondWith(
+    caches.match(e.request).then((cache) => {
+      return cache || fetch(e.request);
+    })
+  );
+});
